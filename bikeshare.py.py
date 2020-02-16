7e6843e5cd19196357714262f984fcea0822334f
zng38 <andreas.friedberger@gmail.com>
2020-02-16 14:43:36 +0100

    bikeshare: load_data function: optimised description

--- bikeshare.py.py
+++ bikeshare.py.py
@@ -64,7 +64,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day if applicable. asdf
 
     Args:
         (str) city - name of the city to analyze
